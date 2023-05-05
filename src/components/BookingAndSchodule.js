f9e6c5b0e59c51a99f7e750788296e8497db983d
ShakilTalukder07 <shakil.talukder07@gmail.com>
2023-05-05 19:14:57 +0600

    project complete

--- src/components/BookingAndSchodule.js
+++ src/components/BookingAndSchodule.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import Schedule from './Schedule';
+import mainland from '../assets/ship_images/mainland.jpg';
+import inland from '../assets/ship_images/inland.jpg';
 
 const BookingAndSchedule = () => {
     return (
         <div>
             <div className="hero">
                 <div className="hero-content flex-col lg:flex-row">
-
-                    <div className='w-full md:w-1/2 lg:w-1/2'>
-                        <div>
-
-                            <div className="card w-96 glass">
-                                <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!" /></figure>
+                    <div className='w-full md:w-1/2 lg:w-1/2 h-full'>
+                        <div className='flex flex-col gap-4'>
+                            <div className="card w-full h-[250px] shadow-xl image-full">
+                                <figure><img src={mainland} className='w-full' alt="Shoes" /></figure>
                                 <div className="card-body">
-                                    <h2 className="card-title">Life hack</h2>
-                                    <p>How to park your car at your garage?</p>
-                                    <div className="card-actions justify-end">
-                                        <button className="btn btn-primary">Learn now!</button>
-                                    </div>
+
+                                    <h2 className="text-3xl text-center my-16">Midland Freight</h2>
+
+                                </div>
+                            </div>
+                            <div className="card w-full h-[250px] shadow-xl image-full">
+                                <figure><img src={inland} className='w-full' alt="Shoes" /></figure>
+                                <div className="card-body">
+
+                                    <h2 className="text-3xl text-center my-16">Inland Freight</h2>
+
                                 </div>
                             </div>
                         </div>
                     </div>
+
                     <div className='w-full md:w-1/2 lg:w-1/2 h-full'>
                         <Schedule></Schedule>
                     </div>
